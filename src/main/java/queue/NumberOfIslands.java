4febd17119ff954a3d57856f36b38b8a7f827786
Jayesh Thakur <jayesh.thakur@lntinfotech.com>
2022-05-06 18:30:56 +0530

    progress on islands problem

---
+++ src/main/java/queue/NumberOfIslands.java
@@ -0,0 +1,120 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Number of Islands leetcode solution with queue BFS
+ */
+public class NumberOfIslands {
+
+    /**
+     * Direction Array for checking all neighbors of
+     * any node
+     */
+    static int[][] dirArr = {{1, 0, -1, 0}, {0, 1, 0, -1}};
+
+    static int row_size, col_size = 0;
+
+    /**
+     * Helper class for each Node
+     */
+    static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * Utility function to check validity of a Node
+     * Checks for out of bounds, already visited or
+     * if it is a water node i.e '0' value
+     *
+     * @param x       x cord of Node
+     * @param y       y cord of node
+     * @param visited visitedArr
+     * @param input   inputArr
+     * @return validity status
+     */
+    private static boolean isValid(int x, int y, boolean[][] visited, char[][] input) {
+        if (x < 0 || y < 0 || x >= row_size || y >= col_size || input[x][y] == '0') return false;
+        return !visited[x][y];
+    }
+
+    public static int numIslands(char[][] grid) {
+
+        int numberOfIslands = 0;
+        row_size = grid.length;
+        col_size = grid[0].length;
+
+        boolean[][] visited = new boolean[row_size][col_size];
+
+        Queue<Node> nodeQueue = new LinkedList<>();
+
+        //adding first node(0,0) to nodeQueue and marking it as visited.
+        nodeQueue.add(new Node(0, 0));
+        visited[0][0] = true;
+
+        while (!nodeQueue.isEmpty()) {
+            //deque
+            Node curNode = nodeQueue.remove();
+
+            int curX = curNode.x;
+            int curY = curNode.y;
+
+            if (curX == 2 && curY == 0)
+            {
+                System.out.println("debug");
+            }
+
+
+            //Checking all adjacent nodes
+
+            boolean island = true;
+            for (int i = 0; i < 4; i++) {
+
+                int adjX = curX + dirArr[0][i];//row
+                int adjY = curY + dirArr[1][i];//col
+
+                if (isValid(adjX, adjY, visited, grid)) {
+                    //If valid adding to queue and marking as visited
+                    nodeQueue.add(new Node(adjX, adjY));
+                    visited[adjX][adjY] = true;
+                }
+                //checking if node is island
+                if (adjX >= 0 && adjY >= 0 && adjX < row_size && adjY < col_size && grid[adjX][adjY] != '0') {
+                    island = false;
+                }
+
+            }
+            if (island) {
+                numberOfIslands++;
+            }
+
+        }
+
+
+        return numberOfIslands;
+    }
+
+
+    //Driver for local
+    public static void main(String[] args) {
+
+
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println(numIslands(grid));
+
+    }
+
+
+}
