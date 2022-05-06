3df27ca0e27fa8191250b47d659cc657c3fc5ac1
Jayesh Thakur <jayesh.thakur@lntinfotech.com>
2022-05-06 19:05:51 +0530

    progress on islands problem

--- src/main/java/queue/NumberOfIslands.java
+++ src/main/java/queue/NumberOfIslands.java
@@ -40,8 +40,20 @@ public class NumberOfIslands {
      * @return validity status
      */
     private static boolean isValid(int x, int y, boolean[][] visited, char[][] input) {
-        if (x < 0 || y < 0 || x >= row_size || y >= col_size || input[x][y] == '0') return false;
+        if (x < 0 || y < 0 || x >= row_size || y >= col_size) return false;
         return !visited[x][y];
+    }
+
+    /**
+     * Check if it is a water node i.e '0' value
+     *
+     * @param x     x cord of Node
+     * @param y     y cord of node
+     * @param input inputArr
+     * @return if it is water node or not
+     */
+    private static boolean isWater(int x, int y, char[][] input) {
+        return input[x][y] == '0';
     }
 
     public static int numIslands(char[][] grid) {
@@ -53,23 +65,21 @@ public class NumberOfIslands {
         boolean[][] visited = new boolean[row_size][col_size];
 
         Queue<Node> nodeQueue = new LinkedList<>();
+        Queue<Node> islandQueue = new LinkedList<>();
 
         //adding first node(0,0) to nodeQueue and marking it as visited.
         nodeQueue.add(new Node(0, 0));
+        islandQueue.add(new Node(0, 0));
         visited[0][0] = true;
 
+        int lastIslandQLength = 0;
         while (!nodeQueue.isEmpty()) {
             //deque
             Node curNode = nodeQueue.remove();
+            if (!islandQueue.isEmpty()) islandQueue.remove();
 
             int curX = curNode.x;
             int curY = curNode.y;
-
-            if (curX == 2 && curY == 0)
-            {
-                System.out.println("debug");
-            }
-
 
             //Checking all adjacent nodes
 
@@ -80,19 +90,27 @@ public class NumberOfIslands {
                 int adjY = curY + dirArr[1][i];//col
 
                 if (isValid(adjX, adjY, visited, grid)) {
+
+                    if (!isWater(adjX, adjY, grid)) {
+                        islandQueue.add(new Node(adjX, adjY));
+                    }
+
                     //If valid adding to queue and marking as visited
                     nodeQueue.add(new Node(adjX, adjY));
                     visited[adjX][adjY] = true;
                 }
+
                 //checking if node is island
-                if (adjX >= 0 && adjY >= 0 && adjX < row_size && adjY < col_size && grid[adjX][adjY] != '0') {
-                    island = false;
-                }
+//                if (adjX >= 0 && adjY >= 0 && adjX < row_size && adjY < col_size && grid[adjX][adjY] != '0') {
+//                    island = false;
+//                }
 
             }
-            if (island) {
+
+            if (lastIslandQLength > 0 && islandQueue.isEmpty()) {
                 numberOfIslands++;
             }
+            lastIslandQLength = islandQueue.size();
 
         }
 
