a986925d4689e27bb73c5259277543412ae849ff
Jayesh Thakur <jayesh.thakur@lntinfotech.com>
2022-05-09 19:13:44 +0530

    progress on lock problem

---
+++ src/main/java/queue/OpenTheLock.java
@@ -0,0 +1,11 @@
+package queue;
+
+public class OpenTheLock {
+
+    public static void main(String[] args) {
+        String test = "0008";
+
+        System.out.println((int)test.charAt(3) - '0');
+
+    }
+}
