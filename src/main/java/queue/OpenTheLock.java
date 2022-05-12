0b6380367484db15dfd9ecc641ccbd1c7d382962
Jayesh <jayeshthakur47@gmail.com>
2022-05-12 09:17:59 +0530

    lock progress

--- src/main/java/queue/OpenTheLock.java
+++ src/main/java/queue/OpenTheLock.java
@@ -1,11 +1,39 @@
 package queue;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class OpenTheLock {
 
-    public static void main(String[] args) {
-        String test = "0008";
 
-        System.out.println((int)test.charAt(3) - '0');
+    private boolean isValid(String input, List<String> deadEnds) {
+        return !deadEnds.contains(input);
+    }
+
+    public static void main(String[] args) {
+        String start = "1239";
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add("0000");
+
+        while (!queue.isEmpty()) {
+
+            String current = queue.remove();
+
+            char[] inputArr = current.toCharArray();
+
+            for (int i = 0; i < 4; i++) {
+
+                int neighbour = (int) inputArr[i] - '0'; // covert using ascii values
+                neighbour--;
+                if (neighbour == -1) neighbour = 9;
+                if (neighbour == 10) neighbour = 0;
+
+
+            }
+        }
+
 
     }
 }
