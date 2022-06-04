1874f10dbdfe9bd9ab3967dc04eb79c64974acf9
Jayesh <jayeshthakur47@gmail.com>
2022-06-04 15:07:01 +0530

    Stack solution much faster

--- src/main/java/stack/DailyTemperature.java
+++ src/main/java/stack/DailyTemperature.java
@@ -1,25 +1,21 @@
 package stack;
 
+import java.util.Stack;
+
 public class DailyTemperature {
 
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
 
+        Stack<Integer> stack = new Stack<>();
+
         for (int i = 0; i < temperatures.length; i++) {
-//            Stack<Integer> stack = new Stack<>();
-            int daysCount = 1;
-            boolean greaterFound = false;
-            for (int j = i + 1; j < temperatures.length; j++) {
-                if (temperatures[j] > temperatures[i]) {
-                    greaterFound = true;
-                    break;
-                } else
-                    daysCount++;
+
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int prev = stack.pop();
+                result[prev] = i - prev;
             }
-            if (greaterFound)
-                result[i] = daysCount;
-            else
-                result[i] = 0;
+            stack.push(i);
         }
         return result;
     }
