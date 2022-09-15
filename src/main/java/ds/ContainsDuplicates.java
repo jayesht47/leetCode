54ad8b94c1814f5b2341cda4824340986f9e1b02
Jayesh <jayeshthakur47@gmail.com>
2022-09-15 19:59:47 +0530

    O(n) mem and time Solution for ContainsDuplicates.java

--- src/main/java/ds/ContainsDuplicates.java
+++ src/main/java/ds/ContainsDuplicates.java
@@ -1,17 +1,29 @@
 package ds;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * https://leetcode.com/problems/contains-duplicate/
  */
 public class ContainsDuplicates {
 
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicateInit(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == nums[i + 1])
                 return true;
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num))
+                return true;
+            else
+                set.add(num);
         }
         return false;
     }
