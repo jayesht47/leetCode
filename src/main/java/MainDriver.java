5e22403daec901b18a5fe986c60c365c65a8ee10
Jayesh <jayeshthakur47@gmail.com>
2022-09-15 12:17:37 +0530

    Init Solution for SearchInsertPosition

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -1,10 +1,13 @@
 import algorithms.BinarySearch;
 import algorithms.IsBadVersion;
+import algorithms.SearchInsertPosition;
 
 public class MainDriver {
 
     public static void main(String[] args) {
-        IsBadVersion isBadVersion = new IsBadVersion();
-        System.out.println(isBadVersion.firstBadVersion(2126753390));
+        SearchInsertPosition s = new SearchInsertPosition();
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println(s.searchInsert(nums, target));
     }
 }
