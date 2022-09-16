88522eb93f8d6671d4b31b8e968dd175da87547c
Jayesh <jayeshthakur47@gmail.com>
2022-09-16 16:33:24 +0530

    O(n) time Solution for twoSum

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 public class MainDriver {
 
     public static void main(String[] args) {
-        MoveZeroes mz = new MoveZeroes();
-        int[] nums = {0,1,0,3,12};
-        Arrays.stream(nums).forEach(System.out::print);
-        System.out.println();
-        mz.moveZeroes(nums);
-        Arrays.stream(nums).forEach(System.out::print);
+        TwoSum2 twoSum = new TwoSum2();
+        int[] nums = {5,25,75};
+        int target = 100;
+        int[] result = twoSum.twoSum(nums, target);
+        Arrays.stream(result).forEach(System.out::print);
     }
 }
