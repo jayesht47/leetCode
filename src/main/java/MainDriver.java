441d7ba10faa8335ee812da6582f7f213053efd3
Jayesh <jayeshthakur47@gmail.com>
2022-09-17 10:55:59 +0530

    O(n/2) time Solution for ReverseStrings

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class MainDriver {
 
     public static void main(String[] args) {
-        TwoSum2 twoSum = new TwoSum2();
-        int[] nums = {5,25,75};
-        int target = 100;
-        int[] result = twoSum.twoSum(nums, target);
-        Arrays.stream(result).forEach(System.out::print);
+        ReverseString rs = new ReverseString();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        rs.reverseString(s);
+        for (char c : s) {
+            System.out.print(c);
+        }
     }
 }
