4cca810253e142e0bb2ec0a8e8039f296d0d27ac
Jayesh <jayeshthakur47@gmail.com>
2023-02-27 16:59:58 +0530

    EncodeDecodeString solution

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -1,26 +1,16 @@
-import arrays.ProductExceptSelf;
-import arrays.ValidSudoku;
+import arrays.EncodeDecodeString;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class MainDriver {
 
     public static void main(String[] args) {
 
-        char[][] board = {{'8', '3', '.', '.', '7', '.', '.', '.', '.'}
-                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
-                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
-                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
-                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
-                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
-                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
-                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
-                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        EncodeDecodeString eds = new EncodeDecodeString();
 
+        String encoded = eds.encode(List.of(new String[]{"Jayesh","Thakur"}));
 
-        ValidSudoku validSudoku = new ValidSudoku();
-
-        System.out.println(validSudoku.isValidSudoku(board));
+        System.out.println(eds.decode(encoded));
 
     }
 }
