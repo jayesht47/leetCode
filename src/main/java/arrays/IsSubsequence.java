189f809bca6f201f598558db57417cd075c8cdde
Jayesh <jayeshthakur47@gmail.com>
2023-09-15 00:57:34 +0530

    IsSubsequence solution

--- src/main/java/arrays/IsSubsequence.java
+++ src/main/java/arrays/IsSubsequence.java
@@ -7,9 +7,9 @@ public class IsSubsequence {
     public boolean isSubsequence(String s, String t) {
         if (s.isEmpty() && t.isEmpty())
             return true;
-        if (s.isEmpty() && !t.isEmpty())
+        if (s.isEmpty())
             return true;
-        if (!s.isEmpty() && t.isEmpty())
+        if (t.isEmpty())
             return false;
         int subSeqCounter = 0;
         for (int i = 0; i < t.length(); i++) {
