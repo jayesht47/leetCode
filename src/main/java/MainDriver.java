c48282cda928cee5869fb7666cf0dbb55a398096
Jayesh Thakur <jayeshthakur47@gmail.com>
2024-10-21 12:35:57 +0530

    updated main to be blank

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -6,12 +6,6 @@ public class MainDriver {
 
     public static void main(String[] args) {
 
-        MinInRotataedAr mir = new MinInRotataedAr();
-
-        int[] nums = {4,5,6,7,0,1,2};
-
-        System.out.println(mir.findMin(nums));
-
 
     }
 }
